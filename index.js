dd4921e72a9ff752eb0373a66ec62f9b90b17d3c
farfai33 <b022210264@student.utem.edu.my>
2023-10-31 12:07:10 +0800

    login api

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ app.use(express.json())
 app.get('/', (req, res) => {
  res.send('Hello world!')
 })
+
+app.post('/login', (req, res) => {
+    // check if username is correct
+    if(req.body.username !='fairus'){
+        return res.status(400).send('Invalid User')
+    }
+    //check if password is correct
+    if(req.body.password !='1234'){
+        return res.status(400).send('Invalid Password')
+    }
+    res.send('login successfully')
+    })
+    
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
 })
